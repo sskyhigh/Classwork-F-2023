f8f3ab55b71737e0e6cbee6daa7cd73518a2bbf0
sskyhigh <windowshao_123@yahoo.com>
2023-12-15 18:25:37 -0500

    writing to file

--- homework8.java
+++ homework8.java
@@ -1,13 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class homework8 {
     public static void main(String[] args) throws FileNotFoundException {
         String filepath = "readFiles/homework8.txt";
+        String output = "OutPutFile/homework8Output.txt";
         List<String> listOfStrings = new ArrayList<String>();
-        // remove all spaces
+        // remove all spaces, commas
         Scanner sc = new Scanner(new FileReader(filepath)).useDelimiter("\\s+");
+        PrintWriter writer = new PrintWriter(output);
         String str;
         // reading till the end of file
         while (sc.hasNext()) {
@@ -18,10 +21,11 @@ public class homework8 {
         // converting this to an array.
         String[] words = listOfStrings.toArray(new String[0]);
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        countDuplicateWords(words, map);
+        countDuplicateWords(words, map, writer);
+        writer.close();
     }
 
-    public static void countDuplicateWords(String[] words, HashMap<String, Integer> map) {
+    public static void countDuplicateWords(String[] words, HashMap<String, Integer> map, PrintWriter writer) {
         for (String word : words) {
             // if word appears more than once
             // add a value since val starts at 1
@@ -32,7 +36,7 @@ public class homework8 {
             }
         }
         for(Map.Entry<String, Integer> s: map.entrySet()){
-            System.out.println(s.getKey() + ": "  + s.getValue());
+            writer.println(s.getKey() + ": "  + s.getValue());
         }
     }
 }
