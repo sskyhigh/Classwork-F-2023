4e825e054a8bbff1623e97e8d43f6b2a03b76e07
sskyhigh <windowshao_123@yahoo.com>
2023-12-15 18:23:44 -0500

    worked on homework 8

--- homework8.java
+++ homework8.java
@@ -6,23 +6,25 @@ public class homework8 {
     public static void main(String[] args) throws FileNotFoundException {
         String filepath = "readFiles/homework8.txt";
         List<String> listOfStrings = new ArrayList<String>();
-        // load content of file based on specific delimiter
+        // remove all spaces
         Scanner sc = new Scanner(new FileReader(filepath)).useDelimiter("\\s+");
         String str;
-        // checking end of file
+        // reading till the end of file
         while (sc.hasNext()) {
             str = sc.next();
             listOfStrings.add(str);
         }
 
+        // converting this to an array.
         String[] words = listOfStrings.toArray(new String[0]);
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         countDuplicateWords(words, map);
-
     }
 
     public static void countDuplicateWords(String[] words, HashMap<String, Integer> map) {
         for (String word : words) {
+            // if word appears more than once
+            // add a value since val starts at 1
             if (map.containsKey(word)) {
                 map.put(word, map.get(word) + 1);
             } else {
